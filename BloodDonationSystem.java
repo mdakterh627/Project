e1c33bdd1a022551842a68f84b1bf6bbd9490829
Md.Akter Hossain <48676203+mdakterh627@users.noreply.github.com>
2019-06-15 09:53:36 +0600

    Add files via upload

---
+++ BloodDonationSystem.java
@@ -0,0 +1,12 @@
+
+package blooddonationsystem;
+
+public class BloodDonationSystem {
+
+    public static void main(String[] args) {
+        MainForm mf = new MainForm();
+        
+        
+    }
+    
+}
